1dec001f2262c03c8c007330e9dea6878013d76f
Riley Shanley <44351771+RileyShanley@users.noreply.github.com>
2018-10-22 21:32:14 -0500

    Initial commit

---
+++ src/main/java/com/ACM/DiscordBot/App.java
@@ -0,0 +1,40 @@
+package com.ACM.DiscordBot;
+
+
+import java.time.ZonedDateTime;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class App extends ListenerAdapter {
+	//private Date party;
+
+	public static void main(String[] args) throws Exception {
+		JDA jda = new JDABuilder(AccountType.BOT).setToken(Ref.token).build();
+		jda.addEventListener(new App());
+
+	}// end of main
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent evt) {
+		// objects
+		User objUser = evt.getAuthor();
+		MessageChannel objMsgCh = evt.getChannel();
+		Message objMsg = evt.getMessage();
+
+		// commands
+		if (objMsg.getContentRaw().equalsIgnoreCase(Ref.prefix + "hi")) {
+			objMsgCh.sendMessage(" !hi").queue();
+		}
+		//if (objMsg.getContentRaw().equalsIgnoreCase(Ref.prefix + "setDate")) {
+		//}
+		if(ZonedDateTime.now().equals(2018/22/10)) {
+			objMsgCh.sendMessage(" it is today").queue();
+		}
+	}
+}
